a3eb13066281a01af21015c1acf8977427e90817
8pro <mechackkobius@gmail.com>
2023-06-24 17:36:55 +0300

    Beyond-Basic-Types done

--- Beyond-Basic-Types/dist/Beyond-Basics-Type.js
+++ Beyond-Basic-Types/dist/Beyond-Basics-Type.js
@@ -1,12 +1,1 @@
 "use strict";
-const getCourse = function (name, price, moreInfos) {
-    return { name, price, moreInfos };
-};
-const course = getCourse("JS Advanced", 19.99, {
-    released: true,
-    beginnerFrendly: true,
-    level: "Beginner-Advanced",
-});
-console.log(course);
-const myName = "Kobius";
-const courseLevel = "Beginner";
--- Beyond-Basic-Types/src/Beyond-Basics-Type.ts
+++ Beyond-Basic-Types/src/Beyond-Basics-Type.ts
@@ -10,7 +10,7 @@
 // console.log(list);
 
 /* ---------------- Indexed Access Types ------------ */
-type Product = {
+/* type Product = {
   name: string;
   price: number;
   moreInfos: {
@@ -37,3 +37,133 @@ console.log(course);
 
 const myName: Product["name"] = "Kobius";
 const courseLevel: Product["moreInfos"]["level"] = "Beginner";
+console.log(courseLevel); */
+
+/* ---------- Partial Type -------------------- */
+// type Product = {
+//   name?: string;
+//   price?: number;
+// };
+// const course: Product = { name: "TS" };
+
+// type Product = {
+//   name: string;
+//   price: number;
+// };
+
+// const course: Partial<Product> = { price: 22 };
+// console.log(course);
+
+// type Product = Partial<{ name: string; price: number }>;
+
+// const course: Product = { price: 22 };
+// console.log(course);
+
+/* ------------- Readonly Type ------------------------ */
+// type Product = Readonly<{ name: string; price: number }>;
+// const course: Product = { name: "JS", price: 50 };
+// // course.name = "TS"
+
+/* ------------- Combine Readonly and Partial Types ------ */
+// type Product = {
+//   name: string;
+//   price: number;
+// };
+
+// const course: Partial<Product> & Readonly<Product> = { name: "TS", price: 22 };
+// console.log(course);
+// course.name = "JS";
+// console.log(course.name);
+/* Does not work */
+
+/* --------------- Shared Fields -------------------- */
+// type Product = {
+//   name: string;
+//   price: number;
+//   sales: number;
+//   availability: boolean;
+//   category: string;
+// };
+// type Course = {
+//   name: string;
+//   price: number;
+//   sales: number;
+//   released: boolean;
+//   rating: number;
+// };
+// const course: Product | Course = {
+//   name: "TS",
+//   price: 22.99,
+//   sales: 200,
+//   availability: true,
+//   category: "online",
+
+//   released: true,
+//   rating: 5.0,
+// };
+
+// /* ---------------------------------- */
+// type DigitalProduct = {
+//   name: string;
+//   price: number;
+//   sales: number;
+// };
+// type Product = DigitalProduct & {
+//   availability: boolean;
+//   category: string;
+// };
+// type Course = DigitalProduct & {
+//   released: boolean;
+//   rating: number;
+// };
+// const course: Product | Course = {
+//   name: "TS",
+//   price: 22.99,
+//   sales: 200,
+//   availability: true,
+//   category: "online",
+
+//   // released: true,
+//   // rating: 5.0,
+// };
+// console.log(course);
+
+/* ---------------------------------- */
+// type DigitalProduct = {
+//   name: string;
+//   price: number;
+//   sales: number;
+// } & (
+//   | { availability: boolean; category: string }
+//   | { released: boolean; rating: number }
+// );
+
+// const course: DigitalProduct = {
+//   name: "TS",
+//   price: 22.99,
+//   sales: 200,
+//   // availability: true,
+//   // category: "online",
+
+//   released: true,
+//   rating: 5.0,
+// };
+// console.log(course);
+
+/* ---------- Optional Chaining Operator ---------- */
+// type Product = {
+//   name: string;
+//   info: ProductInfo | undefined;
+// };
+// type ProductInfo = {
+//   level: string;
+//   noOfStudent: number | undefined;
+// };
+
+// const courses: Product[] = [
+//   { name: "TS", info: { level: "Beginner", noOfStudent: 3000 } },
+//   { name: "JS", info: { level: "Advanced", noOfStudent: undefined } },
+//   { name: "Python", info: undefined },
+// ];
+
+// console.log(courses.map((course) => course.info?.noOfStudent));
