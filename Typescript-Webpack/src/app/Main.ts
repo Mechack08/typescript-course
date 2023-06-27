4f9b972d908f83c8ece53cf3cb253bbd7582c2ef
8pro <mechackkobius@gmail.com>
2023-06-27 13:22:45 +0300

    Typescript-Webpack done

---
+++ Typescript-Webpack/src/app/Main.ts
@@ -0,0 +1,6 @@
+export function getString(): string {
+  return "Hello Webpack";
+}
+export function sayHello(name: string): string {
+  return `Hello ${name}`;
+}
---
+++ Typescript-Webpack/src/index.ts
@@ -0,0 +1,10 @@
+let str: string = "Hello word!";
+
+// Extending Recommended Configurations
+// https://github.com/tsconfig/bases
+
+// export let courseName: string = "TS";
+
+import { getString, sayHello } from "./app/Main";
+console.log(getString());
+console.log(sayHello("Mechack"));
